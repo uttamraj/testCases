dbe30df0da83bd549e2e7e3d1f5260bd72f64507
uttam raj <uttamrajsali@gmail.com>
2013-11-21 14:35:19 -0800

    commit on 11/21/2013

--- New/src/main/java/com/nuance/newFrame/New/App.java
+++ New/src/main/java/com/nuance/newFrame/New/App.java
@@ -1,4 +1,4 @@
-package com.nuance.TestFrame.TestFrame;
+package com.nuance.newFrame.New;
 
 /**
  * Hello world!
@@ -8,9 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String x = args[0];
+    	//String x = args[0];
         System.out.println( "Hello World!" );
-        ProcessXMLFile y = new ProcessXMLFile();
-        y.readXml(x);
+        //ProcessXMLFile y = new ProcessXMLFile();
+        //y.readXml(x);
     }
 }
